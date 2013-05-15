9f6ac012396e7c76b4f00fe22bcb758f74fba46d
Mayleen Lacouture <maylelacouture@gmail.com>
2013-05-15 18:54:19 +0200

    Web Interface (ezb-web)
    - Refactoring and commenting

--- app/controllers/EzbForms.scala
+++ app/controllers/EzbForms.scala
@@ -3,7 +3,6 @@ package controllers
 import models._
 import users.dal._
 import books.dal._
-import utils.Implicits._
 
 import play.api.data._
 import format.Formats._
@@ -20,21 +19,6 @@ import play.api.libs.json.JsValue
 
 object EzbForms {
 
-  val userForm = Form(
-    mapping(
-      "username" -> text,
-      "mail" -> email,
-      "password" -> text
-    )((username, email, password) => User(java.util.UUID.randomUUID(), username, email, password))
-     ((user: User) => Some(user.name, user.email, ""))
-  )
-
-  val loginForm = Form(
-    tuple(
-      "id" -> text,
-      "password" -> text
-  ))
-
   val bookForm = Form[Book](
     mapping(
       "title" -> text,
@@ -46,15 +30,8 @@ object EzbForms {
       "summary" -> text
     )((title,authors,languages,publishers,published_dates,tags,summary) =>
       Book(null,title,authors,languages,publishers,published_dates, tags, summary, List[BookPart]()))
-    ((book:Book) => Some(book.bookTitle, book.bookAuthors, book.bookLanguages, book.bookPublishers, book.bookPublishedDates,
-                      book.bookTags, book.bookSummary))
-  )
-
-  val memberForm = Form(
-    tuple(
-      "member id" -> text,
-      "role" -> text
-    )
+      ((book:Book) => Some(book.bookTitle, book.bookAuthors, book.bookLanguages, book.bookPublishers, book.bookPublishedDates,
+        book.bookTags, book.bookSummary))
   )
 
   def ezoomlayerForm(ezoombookid:UUID, layerid:UUID, userid:UUID) = Form[EzoomLayer](
