9c42cb04623997d1f288c1c2d1c6a4cd88a945c5
Mayleen Lacouture <maylelacouture@gmail.com>
2013-06-04 19:26:32 +0200

    Boostrap and Less
    - Add element on the nav-bar for log-in or my-account parameters
    - Tried to add icons but it's not working

---
+++ ezb_web/app/models/Context.scala
@@ -0,0 +1,12 @@
+package models
+
+import users.dal.User
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mayleen
+ * Date: 04/06/13
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+case class Context (user: Option[String])
