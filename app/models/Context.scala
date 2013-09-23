d2a59a2aee0abc0cc96b0afda3696b1c06088438
Mayleen Lacouture <maylelacouture@gmail.com>
2013-09-23 16:24:02 +0200

    Internationalization
    - Add language configuration to top menu
    - Begin of message externalization
    - Pages affected so far: top-menu, index, book list, book details

--- app/models/Context.scala
+++ app/models/Context.scala
@@ -3,6 +3,8 @@ package models
 import users.dal._
 import books.dal.{Ezoombook,EzoomLayer}
 
+import play.api.i18n.Lang
+
 /**
  * Created with IntelliJ IDEA.
  * User: mayleen
@@ -12,6 +14,8 @@ import books.dal.{Ezoombook,EzoomLayer}
  */
 case class Context (user: Option[User],
                     preferences: Option[Preferences],
+                    supportedLanguages:Seq[Lang],
                     activeEzb:Option[Ezoombook], /* TODO change to ref instead (remove from cache) */
                     activeLayer:Option[EzoomLayer]) /* TODO change to ref idem */
+
 
