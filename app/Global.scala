59b45ae8a8c238d5b26d5f26d5fdac9971756e1d
Mayleen Lacouture <maylelacouture@gmail.com>
2013-12-19 09:15:15 +0100

    Bug Fixing. Solves Issues #11, #10,#2
    - Change some prinlns to Logs
    - Keep connected user in cache for authentication for avoiding query the DB each time
    - Fix book list layout problem in Firefox
    - Fix repeated contribution-id on layer edition form
    - Publish ezb when publishing one of its layers
    - Catch and notify invalid encoding error while loading ezoomlayer from non UTF-8 file
    - Disply friendly message on internal server errors
    - Correct missing image on index page
    - Fix "Add Summary" not working in ezb edition page
    - Add notification on login error
    
    Contained in branches: HEAD, hotfixes, master, origin/master

--- app/Global.scala
+++ app/Global.scala
@@ -7,7 +7,7 @@ import play.api.mvc.Results._
 import users.dal._
 import play.api.Application
 import slick.session.Session
-
+import scala.concurrent.Future
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,8 +19,6 @@ import slick.session.Session
 
 object Global extends DalSettings with DBeable{
 
-//  override def onStart(app: Application) { /* Does nothing */ }
-
   override def onStart(app: Application) {
     implicit val application = app
     lazy val database = getDb
@@ -28,7 +26,7 @@ object Global extends DalSettings with DBeable{
     database.withSession {
       implicit session: Session =>
         dal.create
-	      println("[INFO] Database Created!")
+        Logger.info("Database Created!")
     }
 
     //TODO Find a way to automatically update table definitions
@@ -41,11 +39,11 @@ object Global extends DalSettings with DBeable{
   override def onStop(app: Application) {
   }
 
-//  override def onError(request: RequestHeader, ex: Throwable) = {
-//    InternalServerError(
-//      views.html.error(ex.toString)
-//    )
-//  }
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    Future.successful(InternalServerError(
+      views.html.error("We are experiencing technical problems. Please try again later.")
+    ))
+  }
 }
 
 
