bdcc31c7f4f73c25d913833563aec30325201a80
Ophir LOJKINE <pere.jobs@gmail.com>
2014-12-11 00:02:59 +0100

    Include dal in this repo

--- project/Build.scala
+++ project/Build.scala
@@ -13,9 +13,9 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     //cache,
-    ("ezb-dal-play" % "ezb-dal-play_2.10" % "2.0-SNAPSHOT")
-      .exclude("org.scala-stm", "scala-stm_2.10.0")
-      .exclude("com.typesafe.play", "*"),
+    //("ezb-dal-play" % "ezb-dal-play_2.10" % "2.0-SNAPSHOT")
+    //  .exclude("org.scala-stm", "scala-stm_2.10.0")
+    //  .exclude("com.typesafe.play", "*"),
     ("jp.t2v" %% "play2-auth"      % "0.11.0").exclude("com.typesafe.play", "*"),
     //("jp.t2v" %% "play2-auth-test" % "0.11.0" % "test").exclude("play", "*"),
     ("org.reactivecouchbase" %% "reactivecouchbase-play" % "0.2-SNAPSHOT").exclude("com.typesafe.play", "*"),
@@ -28,12 +28,16 @@ object ApplicationBuild extends Build {
     "ReactiveCouchbase Releases" at "https://raw.github.com/ReactiveCouchbase/repository/master/releases/"
   )
 
+  // Data application layer
+  val ezb_dal_play = Project(id="ezb-dal-play", base=file("dal/ezb_dal_play/dal_bridge"))
+  val ezb_dal = Project(id="ezb-dal", base=file("dal/ezb_dal"))
+
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here    
     templatesImport ++= Seq("users.dal._","books.dal._", "project.dal._", "ezb.comments._", "forms._") ,
     lessEntryPoints <<= baseDirectory(customLessEntryPoints),
     resolvers ++= appResolvers
-  )
+  ) dependsOn(ezb_dal, ezb_dal_play)
 
   // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
   def customLessEntryPoints(base: File): PathFinder =
